574a87410569f2448b22c4915bed1ad46c9cef4e
陆逊 <carefree527@163.com>
2018-01-09 16:15:00 +0800

    hot reload

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,33 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <AppContainer>
+    <App />
+  </AppContainer>,
   document.getElementById('root') as HTMLElement
 );
+
+interface Hot {
+  accept(path: string, cb: Function): void;
+}
+interface Module extends NodeModule {
+  hot: Hot;
+}
+
+if ((module as Module).hot) {
+  (module as Module).hot.accept('./App.tsx', () => {
+      const NextApp = require('./App').default;
+      ReactDOM.render(
+          <AppContainer>
+              <NextApp />
+          </AppContainer>,
+          document.getElementById('root')
+      );
+  });
+}
+
 registerServiceWorker();
